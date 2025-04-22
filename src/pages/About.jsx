d7d99ce01970bc5e8a3fb4b7e7494891d25745ed
eintoci <146792860+eintoci@users.noreply.github.com>
2025-04-22 15:53:21 -0700

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 function About() {
     return (
-        <div>
+        <div className="p-8">
             <div className="flex">
-                <div>
-                    <h1>
+                <div className="mr-8">
+                    <h1 className="text-6xl text-big_text font-cram font-bold mb-20">
                         Get to Know Me
                     </h1>
-                    <p>I’m a creative designer pursuing UXUI research, design and related artistic fields. Currently I’m finishing my degree in Interdisciplinary Computing and the Arts at UC San Diego. :)I’m a creative designer pursuing UXUI research, design and related artistic fields. Currently I’m finishing my degree in Interdisciplinary Computing and the Arts at UC San Diego. :)</p>
+                    <p className="font-josefin text-2xl text-body_text leading-none">
+                        The first thing to know about me is that I love art! I'm studying interdisciplinary computing and the arts as well as congitive science at UC San Diego. I spend my time outside of class hosting the radio show digimeow every monday on UCSD's KSDT radio station. I also spend my time hiking around San Diego and the San Francisco Bay Area, where I grew up. Thanks for taking the time to check out my website!
+                    </p>
+                    <div className="flex justify-center mt-12 mb-16">
+            <a href="https://www.linkedin.com/in/eva-intoci-784461305/">
+                <button className="bg-slate-400 text-white font-josefin px-6 py-3 rounded-2xl hover:bg-big_text ">
+                View my LinkedIn
+                </button>
+            </a>
+            </div>
                 </div>
                 <div>
-                    <img src="" alt="" className="w-100 h-100" />
+                    <img src="/images/about.png"  className="w-[2000px] h-[300px] object-cover mb-40" />
                 </div>
             </div>
         </div>
-    )
+    );
 }
+
+export default About;
